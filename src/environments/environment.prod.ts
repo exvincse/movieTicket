1fd34f6b8351d4a051d455c78753b8b6e21338b2
yang <exvincse@gmail.com>
2025-03-07 17:30:31 +0800

    修改環境變數

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,7 @@
 export const environment = {
     production: true,
-    //apiUrl: "https://localhost:44341/api",
-    apiUrl: "https://movietickapi.azure-api.net/v1/api",
-    apiKey: "",
-    imgUrl: "",
-    enableMocking: false
+    apiUrl: "https://movietickapi20250304131132.azurewebsites.net/api",
+    azureSubKey: "94da4ce3069a4d1ab952d5cb142d2640"
 };
 
 export const environmentTMDB = {
@@ -12,6 +9,4 @@ export const environmentTMDB = {
     apiUrl: "https://api.themoviedb.org/3",
     // eslint-disable-next-line max-len
     apiKey: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2RmZDQ4ZWY4YTlmNjNjYjQ5Zjg2Nzk2OTVmMzQ5MyIsIm5iZiI6MTU3MjA3NDAyOS44MSwic3ViIjoiNWRiM2YyMmQyNWE1MzYwMDEzZTFkZGI1Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.6OC7Zgh61UeA1gj0C2qr4doBmFftjvKkiE51idHOUfo",
-    imgUrl: "",
-    enableMocking: false
 };
